243524f8074c045d3dcb6e338e30af5d2708fe2c
Johny Ho <johny@thoughtbot.com>
2020-11-02 21:09:25 -0500

    Use a HSL slider for saturation
    This commit converts the HSV slider to a HSL slider for saturation.

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -5,11 +5,10 @@ import {
   SliderType,
   sliderDefaultOptions,
   resolveSvgUrl,
-  getSliderDimensions, 
-  getSliderValueFromInput, 
-  getSliderHandlePosition, 
-  getSliderGradient,
+  getSliderDimensions,
+  getSliderValueFromInput,
   getSliderGradientCoords,
+  SliderOptions,
 } from '@irojs/iro-core';
 
 import { IroComponentBase, IroComponentProps, IroInputType } from './ComponentBase';
@@ -21,6 +20,128 @@ interface IroSliderProps extends IroComponentProps {
   minTemperature: number;
   maxTemperature: number;
 };
+/**
+ * @desc Get the gradient stops for a slider
+ * @param props - slider props
+ * @param color
+ */
+export function getSliderGradient(props: Partial<SliderOptions>, color: IroColor) {
+  const hsv = color.hsv;
+  const rgb = color.rgb;
+
+  switch (props.sliderType) {
+    case 'red':
+      return [
+        [0, `rgb(${ 0 },${ rgb.g },${ rgb.b })`],
+        [100, `rgb(${ 255 },${ rgb.g },${ rgb.b })`],
+      ];
+    case 'green':
+      return [
+        [0, `rgb(${ rgb.r },${ 0 },${ rgb.b })`],
+        [100, `rgb(${ rgb.r },${ 255 },${ rgb.b })`],
+      ];
+    case 'blue':
+      return [
+        [0, `rgb(${ rgb.r },${ rgb.g },${ 0 })`],
+        [100, `rgb(${ rgb.r },${ rgb.g },${ 255 })`],
+      ];
+    case 'alpha':
+      return [
+        [0, `rgba(${ rgb.r },${ rgb.g },${ rgb.b },0)`],
+        [100, `rgb(${ rgb.r },${ rgb.g },${ rgb.b })`],
+      ];
+    case 'kelvin':
+      const stops = [];
+      const min = props.minTemperature;
+      const max = props.maxTemperature;
+      const numStops = 8;
+      const range = max - min;
+      for (let kelvin = min, stop = 0; kelvin < max; kelvin += range / numStops, stop += 1) {
+        const { r, g, b } = IroColor.kelvinToRgb(kelvin);
+        stops.push([ 100 / numStops * stop, `rgb(${r},${g},${b})` ]);
+      }
+      return stops;
+    case 'hue':
+      return [
+        [0,      '#f00'],
+        [16.666, '#ff0'],
+        [33.333, '#0f0'],
+        [50,     '#0ff'],
+        [66.666, '#00f'],
+        [83.333, '#f0f'],
+        [100,    '#f00'],
+      ];
+    case 'saturation':
+      const noSat = {h: color.hsl.h, s: 0, l: color.hsl.l};
+      const fullSat = {h: color.hsl.h, s: 100, l: color.hsl.l};
+      return [
+        [0, `hsl(${noSat.h},${noSat.s}%,${noSat.l}%)`],
+        [100, `hsl(${fullSat.h},${fullSat.s}%,${fullSat.l}%)`]
+      ];
+    case 'value':
+    default:
+      const hsl = IroColor.hsvToHsl({h: hsv.h, s: hsv.s, v: 100});
+      return [
+        [0, '#000'],
+        [100, `hsl(${hsl.h},${hsl.s}%,${hsl.l}%)`]
+      ];
+  }
+}
+
+
+/**
+ * @desc Get the current slider value for a given color, as a percentage
+ * @param props - slider props
+ * @param color
+ */
+export function getCurrentSliderValue(props: Partial<SliderOptions>, color: IroColor) {
+  const hsva = color.hsva;
+  const rgb = color.rgb;
+  const hsl = color.hsl;
+
+  switch (props.sliderType) {
+    case 'red':
+      return rgb.r / 2.55;
+    case 'green':
+      return rgb.g / 2.55;
+    case 'blue':
+      return rgb.b / 2.55;
+    case 'alpha':
+      return hsva.a * 100;
+    case 'kelvin':
+      const { minTemperature, maxTemperature } = props;
+      const temperatureRange = maxTemperature - minTemperature;
+      const percent = ((color.kelvin - minTemperature) / temperatureRange) * 100;
+      // clmap percentage
+      return Math.max(0, Math.min(percent, 100));
+    case 'hue':
+      return hsva.h /= 3.6;
+    case 'saturation':
+      return hsl.s;
+    case 'value':
+    default:
+      return hsva.v;
+  }
+}
+
+
+/**
+ * @desc Get the current handle position for a given color
+ * @param props - slider props
+ * @param color
+ */
+export function getSliderHandlePosition(props: Partial<SliderOptions>, color: IroColor) {
+  const { width, height, handleRange, handleStart } = getSliderDimensions(props);
+  const ishorizontal = props.layoutDirection === 'horizontal';
+  const sliderValue = getCurrentSliderValue(props, color);
+  const midPoint = ishorizontal ? width / 2 : height / 2;
+  let handlePos = handleStart + (sliderValue / 100) * handleRange;
+  if (ishorizontal) {
+    handlePos = -1 * handlePos + handleRange + handleStart * 2;
+  }
+  return {x: ishorizontal ? midPoint : handlePos, y: ishorizontal ? handlePos : midPoint};
+}
+
 
 export function IroSlider(props: IroSliderProps) {
   const activeIndex = props.activeIndex;
@@ -33,14 +154,21 @@ export function IroSlider(props: IroSliderProps) {
   function handleInput(x: number, y: number, type: IroInputType) {
     const value = getSliderValueFromInput(props, x, y);
     props.parent.inputActive = true;
-    activeColor[props.sliderType] = value;
+    const hsl = activeColor.hsl;
+
+    if (props.sliderType === "saturation") {
+      activeColor.hsl = {h: hsl.h, s: value, l: hsl.l};
+    } else {
+      activeColor[props.sliderType] =  value;
+    }
+
     props.onInput(type);
   }
 
   return (
     <IroComponentBase {...props} onInput={ handleInput }>
       {(uid, rootProps, rootStyles) => (
-        <svg 
+        <svg
           { ...rootProps }
           className="IroSlider"
           width={ width }
@@ -67,13 +195,13 @@ export function IroSlider(props: IroSliderProps) {
               </pattern>
             )}
           </defs>
-          <rect 
+          <rect
             className="IroSliderBg"
-            rx={ radius } 
-            ry={ radius } 
-            x={ props.borderWidth / 2 } 
-            y={ props.borderWidth / 2 } 
-            width={ width - props.borderWidth } 
+            rx={ radius }
+            ry={ radius }
+            x={ props.borderWidth / 2 }
+            y={ props.borderWidth / 2 }
+            width={ width - props.borderWidth }
             height={ height - props.borderWidth }
             stroke-width={ props.borderWidth }
             stroke={ props.borderColor }
